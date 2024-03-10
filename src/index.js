4eb13c0687b820f0d112abb557bfbaa36f5264de
Nishant Mohan <128303476+Nishant040305@users.noreply.github.com>
2024-03-10 14:37:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './components_n/components/Quiz/quiz.css';
+import './components_n/components/Quiz/sidebar.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
