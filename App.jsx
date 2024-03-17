c5b60a9a2e281eb5b8c98f70a16335054b9e0d72
souvik-sarkar04 <148918451+souvik-sarkar04@users.noreply.github.com>
2024-03-18 00:10:36 +0530

    DASHBOARD SOUVIK 18/3/24

--- App.jsx
+++ App.jsx
@@ -6,10 +6,10 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';  // Import Bootst
 import 'boxicons/css/boxicons.min.css';         // Import Boxicons CSS
 // import 'react-bootstrap-sidebar-menu/dist/sidebar-menu.scss'
 // import './component_s/classroom.css';
-import SideBar from './component_s/sidebar';
+// import SideBar from './component_s/sidebar';
 
-import Classroom from './component_s/Classroom';
-import Dashboard from './component_s/dashboard';
+// import Classroom from './component_s/Classroom';
+// import Dashboard from './component_s/dashboard';
 // import { Route, Routes } from 'react-router-dom';
 import Page from './component_s/Page3/Page3';
 import Page2 from './component_s/Page2/Page2';
@@ -17,7 +17,7 @@ import Page2 from './component_s/Page2/Page2';
 // import Sidebar from './component_s/Sidebar'
 
 import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
-
+import Homepage from './component_s/Page1/Classroom';
 
 import Home from "./component_s/Page3/Home"
 // import Login from "./component_s/Page3/Login"
@@ -31,57 +31,20 @@ import { useState } from 'react';
 
 function App() {
   return (
-
     //  <div className="App">
-    {/* <Page/> */ }
 
 
     //Page 3 code
-    , {/* <Router>
      <Routes>
-      <Route path="/" element={<Page/>}/>
+      <Route path="/" element={<Homepage/>}/>
       </Routes>
-     </Router>
-    </div>  */}
+   
 
 
 
 
 
     //Page 1 Code
-    // <div className="App">
-
-
-    //   <div className="wrapper">
-    //      <div className="container">
-
-    //        <div className="sidebar"><SideBar/></div>
-
-    //       <div className='flex'>
-    //       <div className='Dashboard'><Dashboard/></div>
-
-    //        <div className="classes">
-
-    //         <Classroom subject_n="Data Structures" teacher_n="Dr. Deepak Gupta" image="Honors.jpg"/>
-
-    //          <Classroom subject_n=" Professional Communication" teacher_n="Dr. Jyotsna Sinha"image="Honors2.jpg"  />
-
-    //          <Classroom subject_n="Health and Hygiene" teacher_n="Dr. Neha Alladuniv" image="Honors.jpg"/>
-    //          <Classroom subject_n="Biotechnology" teacher_n="Dr. Monica" image="Honors2.jpg"/>
-    //         <Classroom subject_n="Programming Paradigms" teacher_n="Dr. Neha Alladuniv"image="Honors.jpg" />
-    //          <Classroom subject_n="Mathematics" teacher_n="Dr. Supriya Yadav" image="Honors3.jpg"/>
-
-    //          <Classroom subject_n="Physics" teacher_n="Dr. Monica"image="Honors2.jpg" />
-    //          <Classroom subject_n=" Environment and Climate Change" teacher_n="Dr. Palak Jain"image="Honors3.jpg" />
-    //          <Classroom subject_n="Chemistry" teacher_n="Dr. Supriya Yadav"image="Honors3.jpg" />
-
-    //        </div>
-
-    //       </div>
-
-    //     </div>
-    //    </div>
-    //  </div> 
 
 
 
